e3b3177269846af33895d094de6a974da5698d51
N. Shimizu <chikoski@gmail.com>
2012-08-29 21:41:25 +0900

    use ClassLoader to access css files in the jar file

--- kantan_reader/src/main/java/net/sqs2/omr/result/export/CSSFileExportModule.java
+++ kantan_reader/src/main/java/net/sqs2/omr/result/export/CSSFileExportModule.java
@@ -1,6 +1,7 @@
 package net.sqs2.omr.result.export;
 
 import java.io.File;
+import java.io.InputStream;
 import java.io.IOException;
 import org.apache.commons.io.FileUtils;
 
@@ -9,14 +10,32 @@ import net.sqs2.omr.app.MarkReaderConstants;
 public class CSSFileExportModule{
 
     protected File origin;
+    protected ClassLoader loader;
 
     public CSSFileExportModule(){
 	this.origin = new File(MarkReaderConstants.USER_CUSTOMIZE_CONSTANTS_DIR, "css");
+	this.loader = getClass().getClassLoader();
     }
 
     public boolean export(File resultDirectoryFile) throws IOException{
+	File cssDirectoryFile = createCSSExportDirectory(resultDirectoryFile);
+	copyCSSFile("base.css", cssDirectoryFile);
+	//	FileUtils.copyDirectory(this.origin, cssDirectoryFile);
+	return true;
+    }
+
+    protected File createCSSExportDirectory(File resultDirectoryFile) throws IOException{
 	File cssDirectoryFile = new File(resultDirectoryFile, "css"); // XXX
-	FileUtils.copyDirectory(this.origin, cssDirectoryFile);
+	cssDirectoryFile.mkdirs();
+	return cssDirectoryFile;
+    }
+
+    protected boolean copyCSSFile(String filename, File cssDirectoryFile) throws IOException{
+	File dest = new File(cssDirectoryFile, filename);
+	InputStream in = this.loader.getResourceAsStream("css/" + filename);
+	if(in != null){
+	    FileUtils.copyInputStreamToFile(in, dest);
+	}
 	return true;
     }
 
